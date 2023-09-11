001645369bd6226ec86c055288749ab93bbf2307
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-09-11 15:37:50 -0400

    Desarrollo del modulo Dba.

---
+++ app/src/main/java/com/example/registropersona/data/local/PersonaDb.kt
@@ -0,0 +1,14 @@
+package com.example.registropersona.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.registropersona.data.local.entities.Persona
+
+@Database(
+    entities = [Persona::class],
+    version = 3,
+    exportSchema = false
+)
+abstract class PersonaDb : RoomDatabase() {
+    abstract fun PersonaDb(): PersonaDb
+}
